1a7040a647e2239b9afafbffc0622b09de11fbfa
Sanket Pote <95067458+sanketsanjaypote29@users.noreply.github.com>
2023-12-27 02:47:30 +0530

    Update ApiRoutes.js

--- client/src/utils/ApiRoutes.js
+++ client/src/utils/ApiRoutes.js
@@ -1,4 +1,6 @@
-export const HOST = "http://localhost:3005";
+export const HOST = process.env.NODE_ENV === 'production'
+  ? 'https://whats-app-clone-29-git-main-sanketsanjaypote29s-projects.vercel.app'
+  : 'http://localhost:3005';
 
 const AUTH_ROUTE = `${HOST}/api/auth`;
 const MESSAGES_ROUTE = `${HOST}/api/messages`;
